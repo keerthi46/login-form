e7da6819037f065efd24e90b56275f53400d6abc
Keerthi D M <keerthiraj143.kr@gmail.com>
2022-07-26 14:15:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import Login from "./pages/Login";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div>
-      <BrowserRouter Basename="/login-form">
+      <HashRouter Basename="/login-form">
         <Route exact path="/login-form" " component={Login} />
         <Route exact path="/" component={Dashboard} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
